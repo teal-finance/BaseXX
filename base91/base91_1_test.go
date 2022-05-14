e587f800ca48253a9e4c6dc177df0d600f18537d
Oliver <0uep@hessling.fr>
2022-05-14 03:34:59 +0200

    ✅ Refactor test and bench of base91

--- base91/base91_1_test.go
+++ base91/base91_1_test.go
@@ -16,44 +16,61 @@
 package base91
 
 import (
+	"bytes"
 	"encoding/hex"
 	"math/rand"
 	"testing"
-	"time"
 
-	acBase91 "github.com/teal-finance/BaseXX/ac/base91"
+	equimBase91 "github.com/Equim-chan/base91-go"
+	bproctorBase91 "github.com/bproctor/base91"
+
+	// breezechenBase91 "github.com/breezechen/base91"
+
+	majestrateBase91 "github.com/majestrate/base91"
+	mtraverBase91 "github.com/mtraver/base91"
+	// acBase91 "github.com/teal-finance/BaseXX/ac/base91"
 )
 
-type testValues struct {
-	dec []byte
-	enc string
-	en2 string // to compare to a different encoder using a different alphabet
-}
+const nn = 11
+const nnn = 1024 // power of two to speed up the % modulo
+var bin [][]byte
+
+const benchChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;<=>?@[]^_`{|}~'"
+//nst benchChar2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;<=>?@[]^_`{|}~\""
+
+var benchEncoding = NewEncoding(benchChars)
+
 
 var tstEncoding = NewEncoding(btcDigits[:Base])
 
-const n = 8192 // power of two to speed up the % modulo
-var testPairs = make([]testValues, 0, n)
+// func init() {
+// 	// If we do not seed the prng - it will default to a seed of (1)
+// 	// https://golang.org/pkg/math/rand/#Seed
+// 	rand.Seed(time.Now().UTC().UnixNano())
+// }
 
-func init() {
-	// If we do not seed the prng - it will default to a seed of (1)
-	// https://golang.org/pkg/math/rand/#Seed
-	rand.Seed(time.Now().UTC().UnixNano())
-}
-
-func initTestPairs() {
-	if len(testPairs) > 0 {
+func setup[T string | []byte](data *[]T, encode func([]byte) T) {
+	if len(*data) > 0 {
 		return
 	}
+	setupBin()
 	// pre-make the test pairs, so it doesn't take up benchmark time...
-	for i := 0; i < n; i++ {
-		data := make([]byte, 32)
-		rand.Read(data)
-		testPairs = append(testPairs, testValues{
-			dec: data,
-			enc: StdEncoding.EncodeToString(data),
-			en2: acBase91.EncodeToString(data),
-		})
+	*data = make([]T, 0, nnn)
+	for i := 0; i < nnn; i++ {
+		str := encode(bin[i])
+		*data = append(*data, str)
+	}
+}
+
+func setupBin() {
+	if len(bin) > 0 {
+		return
+	}
+	bin = make([][]byte, 0, nnn)
+	for i := 0; i < nnn; i++ {
+		b := make([]byte, rand.Intn(nn))
+		rand.Read(b)
+		bin = append(bin, b)
 	}
 }
 
@@ -142,38 +159,221 @@ func testEncDecLoop(t *testing.T, enc *Encoding) {
 	}
 }
 
-func BenchmarkEncode(b *testing.B) {
-	initTestPairs()
-	b.ResetTimer()
+var sAscii []string
 
-	for i := 0; i < b.N; i++ {
-		StdEncoding.Encode(testPairs[i%n].dec)
+func TestBDecode(t *testing.T) {
+	setup(&sAscii, benchEncoding.EncodeToString)
+
+	for i := 0; i < nnn; i++ {
+		b, err := benchEncoding.DecodeString(sAscii[i])
+		if err != nil {
+			t.Fatalf("#%d err=%v ascii=%v", i, err, sAscii[i])
+		}
+		if bytes.Compare(b, bin[i]) != 0 {
+			t.Errorf("#%d ascii: %v", i, sAscii[i])
+			t.Errorf("#%d want: %x", i, bin[i])
+			t.Errorf("#%d got : %x", i, b)
+			t.FailNow()
+		}
 	}
 }
 
-func BenchmarkEncodeACBase91(b *testing.B) {
-	initTestPairs()
+func BenchmarkEncode(b *testing.B) {
+	setupBin()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		acBase91.EncodeToString(testPairs[i%n].dec)
+		benchEncoding.Encode(bin[i%nnn])
 	}
 }
 
 func BenchmarkDecode(b *testing.B) {
-	initTestPairs()
+	setup(&sAscii, benchEncoding.EncodeToString)
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		_, _ = StdEncoding.DecodeString(testPairs[i%n].enc)
+		_, _ = benchEncoding.DecodeString(sAscii[i%nnn])
+	}
+}
+
+/*
+var acAscii [][]byte
+
+func TestBDecodeACBase91(t *testing.T) {
+	setup(&acAscii, acBase91.Encode)
+	for i := 0; i < n; i++ {
+		b := acBase91.Decode(acAscii[i])
+		if bytes.Compare(b, bin[i]) != 0 {
+			t.Errorf("#%d ascii: %v", i, string(acAscii[i]))
+			t.Errorf("#%d want: %x", i, bin[i])
+			t.Errorf("#%d got : %x", i, b)
+			t.FailNow()
+		}
+	}
+}
+
+func BenchmarkEncodeACBase91(b *testing.B) {
+	setupBin()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		acBase91.Encode(bin[i%n])
 	}
 }
 
 func BenchmarkDecodeACBase91(b *testing.B) {
-	initTestPairs()
+	setup(&dataValues, benchEncoding)
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		_ = acBase91.DecodeToString([]byte(testPairs[i%n].en2))
+		_ = acBase91.Decode(dataValues[i%n].bAscii)
+	}
+}
+*/
+
+var bproctorAscii [][]byte
+
+func TestBDecodeBproctorBase91(t *testing.T) {
+	setup(&bproctorAscii, bproctorBase91.Encode)
+	for i := 0; i < nnn; i++ {
+		b := bproctorBase91.Decode(bproctorAscii[i])
+		if bytes.Compare(b, bin[i]) != 0 {
+			t.Errorf("#%d ascii: %v", i, bproctorAscii[i])
+			t.Errorf("#%d want: %x", i, bin[i])
+			t.Errorf("#%d got : %x", i, b)
+			t.FailNow()
+		}
+	}
+
+}
+
+func BenchmarkDecodeBproctorBase91(b *testing.B) {
+	setup(&bproctorAscii, bproctorBase91.Encode)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = bproctorBase91.Decode(bproctorAscii[i%nnn])
+	}
+}
+
+/*
+var breezechenEncoding = breezechenBase91.StdEncoding // breezechenBase91.NewEncoding(benchChars)
+var breezechenAscii [][]byte
+
+func TestBDecodeBreezechenBase91(t *testing.T) {
+	a := make([]byte, nn*2)
+	setup(&breezechenAscii, func(b []byte) []byte {
+		n := breezechenEncoding.Encode(a, b)
+		var out []byte
+		copy(out, a[:n])
+		return out
+	})
+
+	b := make([]byte, nn*2)
+	for i := 0; i < nnn; i++ {
+		n, err := breezechenEncoding.Decode(b, breezechenAscii[i])
+		if err != nil {
+			t.Fatalf("#%d err=%v ascii=%v", i, err, string(breezechenAscii[i]))
+		}
+		if bytes.Compare(b[:n], bin[i]) != 0 {
+			t.Errorf("#%d ascii: %v", i, breezechenAscii[i])
+			t.Errorf("#%d want: %x", i, bin[i])
+			t.Errorf("#%d got : %x", i, b[:n])
+			t.FailNow()
+		}
+	}
+}
+
+func BenchmarkDecodeBreezechenBase91(b *testing.B) {
+	setup(&dataValues, benchEncoding)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, _ = breezechenEncoding.Decode(nil, dataValues[i%n].bAscii)
+	}
+}
+*/
+
+var equimAscii []string
+
+func TestBDecodeEquimBase91(t *testing.T) {
+	setup(&equimAscii, equimBase91.EncodeToString)
+	for i := 0; i < nnn; i++ {
+		b := equimBase91.DecodeString(equimAscii[i])
+		if bytes.Compare(b, bin[i]) != 0 {
+			t.Errorf("#%d ascii: %v", i, equimAscii[i])
+			t.Errorf("#%d want: %x", i, bin[i])
+			t.Errorf("#%d got : %x", i, b)
+			t.FailNow()
+		}
+	}
+
+}
+
+func BenchmarkDecodeEquimBase91(b *testing.B) {
+	setup(&equimAscii, equimBase91.EncodeToString)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		equimBase91.DecodeString(equimAscii[i%nnn])
+	}
+}
+
+var majestrateAscii [][]byte
+
+func TestBDecodeMajestrateBase91(t *testing.T) {
+	setup(&majestrateAscii, majestrateBase91.Encode)
+	for i := 0; i < nnn; i++ {
+		b, err := majestrateBase91.Decode(majestrateAscii[i])
+		if err != nil {
+			t.Fatalf("#%d err=%v ascii=%v", i, err, majestrateAscii[i])
+		}
+		if bytes.Compare(b, bin[i]) != 0 {
+			t.Errorf("#%d ascii: %v", i, majestrateAscii[i])
+			t.Errorf("#%d want: %x", i, bin[i])
+			t.Errorf("#%d got : %x", i, b)
+			t.FailNow()
+		}
+	}
+}
+
+func BenchmarkDecodeMajestrateBase91(b *testing.B) {
+	setup(&majestrateAscii, majestrateBase91.Encode)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, _ = majestrateBase91.Decode(majestrateAscii[i%nnn])
+	}
+}
+
+var mtraverEncoding = mtraverBase91.NewEncoding(benchChars)
+var mtraverAscii []string
+
+func TestBDecodeMtraverBase91Std(t *testing.T) {
+	setup(&mtraverAscii, mtraverEncoding.EncodeToString)
+	if len(mtraverAscii) == 0 {
+		t.Fatal("len=0")
+	}
+
+	for i := 0; i < nnn; i++ {
+		b, err := mtraverEncoding.DecodeString(mtraverAscii[i])
+		if err != nil {
+			t.Fatalf("#%d err=%v ascii=%v", i, err, mtraverAscii[i])
+		}
+		if bytes.Compare(b, bin[i]) != 0 {
+			t.Errorf("#%d len=%d ascii: %v", i, len(mtraverAscii[i]), mtraverAscii[i])
+			t.Errorf("#%d len=%d want: %x", i, len(bin[i]), bin[i])
+			t.Errorf("#%d len=%d got : %x", i, len(b), b)
+			t.FailNow()
+		}
+	}
+}
+
+func BenchmarkDecodeMtraverBase91(b *testing.B) {
+	setup(&mtraverAscii, mtraverEncoding.EncodeToString)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, _ = mtraverEncoding.DecodeString(mtraverAscii[i%nnn])
 	}
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,12 @@ go 1.18
 
 require github.com/mr-tron/base58 v1.2.0
 
-require github.com/unix-world/smartgo v0.0.0-20220422190424-359fb51071e3
+require (
+	github.com/Equim-chan/base91-go v0.2.3
+	github.com/bproctor/base91 v0.0.0-20160902125316-7de6f1dd34e8
+	github.com/majestrate/base91 v0.0.0-20160406115715-9911d3d6c2aa
+	github.com/mtraver/base91 v1.0.0
+	github.com/unix-world/smartgo v0.0.0-20220509170635-0a410492b76e
+)
+
+require ekyu.moe/base91 v0.2.3 // indirect
